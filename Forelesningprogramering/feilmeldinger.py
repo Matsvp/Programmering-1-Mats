b77421cbde01cedc115dbcce948e90d1211b4a63
Matsvp <matsvp@hiof.no>
2024-08-30 11:48:07 +0200

    feilmeldinger

---
+++ Forelesningprogramering/feilmeldinger.py
@@ -0,0 +1,20 @@
+
+#ZeroDivisionError
+#deler på null går ikke
+#a = 42/0
+#print(a)
+
+#NameError 
+#Variablene sammsvarer ikke en er skrevet feil
+#sitat = 42
+#print(sittatt)
+
+# ValueError
+# Prøver å gjøre tekst om til tall
+#tekst = "Dette er en tekst"
+#nummer = int(tekst)
+
+#TypeError
+planeter = "anntall planeter i solsystemet"
+antall_planeter = 8
+print(planeter + antall_planeter)
