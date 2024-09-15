9cebc532823d73f6fce0d297785ee327653590f8
Matsvp <matsvp@hiof.no>
2024-09-16 00:07:53 +0200

    fin puss på oblig

--- Oblig_2/oppgave_6.py
+++ Oblig_2/oppgave_6.py
@@ -1,15 +1,42 @@
-# Skriv L for å legge til
-# Skriv T for å ta bort noe
-# Skriv H for å skrive ut listen
-liste []
+# Start med en tom liste
+liste = []
 
-komando = input('Skriv "L" for å legge til ting , Skriv "T" for å fjerne ting, Skriv "H" for å vise hele listen.')
-for liste in range():
-    if komando == "L": 
-        print("hva vil du legge til")
+# Kjør en løkke som fortsetter til brukeren velger å stoppe
+while True:
+    # Tar inn kommandoen fra brukeren
+    komando = input('Skriv "L" for å legge til ting, "T" for å fjerne ting, "H" for å vise hele listen, og "stopp" for å stoppe programmet.')
 
-    elif komando == "T":
-        print("hva vil du ta bort")
+    # Sjekk hva brukeren vil gjøre
+    if komando == "L" or "l": 
+        # Hvis brukeren vil legge til noe, spør etter det
+        ting = input("Hva vil du legge til? ")
+        liste.append(ting)  # Legger til elementet i listen
+        print(f'"{ting}" er lagt til i listen.')
 
-    else komando == "H":
-        print("her er listen")
+    elif komando == "T" or "t":
+        # Hvis brukeren vil fjerne noe, spør etter hva de vil fjerne
+        ting = input("Hva vil du ta bort fra listen? ")
+        if ting in liste:
+            liste.remove(ting)  # Fjerner elementet fra listen hvis det finnes
+            print(f'"{ting}" er fjernet fra listen.')
+        else:
+            # Hvis elementet ikke finnes i listen
+            print(f'"{ting}" finnes ikke i listen.')
+
+    elif komando == "H" or "h":
+        # Hvis brukeren vil se hele listen
+        print("Her er listen:")
+        for H in liste:
+            print(H)  # Skriver ut hvert element i listen
+
+    elif komando.lower() == "stopp" or "Stopp":
+        # Hvis brukeren vil stoppe programmet
+        print("Programmet er ferdig.")
+        print(f'Her er listen: {liste} i tilfelle du trenger det senere :)')
+        break  # Avslutter løkken
+
+    else:
+        # Hvis brukeren skriver noe annet enn "L", "T", "H", eller "stopp"
+        print('Ugyldig kommando. Vennligst skriv "L", "T", "H", eller "stopp".')
+    
+    print()  # Legger til en tom linje for å gjøre utdataene mer oversiktlige
